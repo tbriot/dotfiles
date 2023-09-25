b70ed4d4714544417c5193378c1d2c86290786c5
Thomas Briot <thomas.briot82@gmail.com>
2023-09-24 21:55:48 -0400

    Load ssh keys

--- zsh/ssh-agent.sh
+++ zsh/ssh-agent.sh
@@ -6,6 +6,14 @@ agent_start () {
     (umask 077; ssh-agent >| "$env")
     . "$env" >| /dev/null ; }
 
+agent_add_keys () {
+  for possiblekey in ${HOME}/.ssh/id_*; do
+    if grep -q PRIVATE "$possiblekey"; then
+      ssh-add "$possiblekey"
+    fi
+  done
+}
+
 agent_load_env
 
 # agent_run_state: 0=agent running w/ key; 1=agent w/o key; 2=agent not running
@@ -13,9 +21,9 @@ agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
 
 if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
     agent_start
-    ssh-add
+    agent-add-keys
 elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
-    ssh-add
+    agent-add-keys
 fi
 
 unset env
