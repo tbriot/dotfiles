61ddb847c567b49c89bbf0ce7c50d6cf9a60ec75
Thomas Briot <thomas.briot82@gmail.com>
2023-09-26 20:26:56 -0400

    Do not load ssh key at startup

--- zsh/ssh-agent.sh
+++ zsh/ssh-agent.sh
@@ -6,14 +6,6 @@ agent_start () {
     (umask 077; ssh-agent >| "$env")
     . "$env" >| /dev/null ; }
 
-agent_add_keys () {
-  for possiblekey in ${HOME}/.ssh/id_*; do
-    if grep -q PRIVATE "$possiblekey"; then
-      ssh-add "$possiblekey"
-    fi
-  done
-}
-
 agent_load_env
 
 # agent_run_state: 0=agent running w/ key; 1=agent w/o key; 2=agent not running
@@ -21,9 +13,6 @@ agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
 
 if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
     agent_start
-    agent-add-keys
-elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
-    agent-add-keys
 fi
 
 unset env
