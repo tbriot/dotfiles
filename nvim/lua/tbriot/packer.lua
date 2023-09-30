b4681d646014d82e01f87b1d350897936ac91cfc
Thomas Briot <thomas.briot82@gmail.com>
2023-09-30 18:30:04 -0400

    Identation in packer.lua

--- nvim/lua/tbriot/packer.lua
+++ nvim/lua/tbriot/packer.lua
@@ -4,23 +4,23 @@
 vim.cmd [[packadd packer.nvim]]
 --
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.3',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  use({ 'rose-pine/neovim', as = 'rose-pine' ,
-  config = function()
-	  vim.cmd('colorscheme rose-pine')
-  end
-     })
+    use({ 'rose-pine/neovim', as = 'rose-pine' ,
+    config = function()
+        vim.cmd('colorscheme rose-pine')
+    end
+})
 
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-    use('theprimeagen/harpoon')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
+use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+use('theprimeagen/harpoon')
+use('mbbill/undotree')
+use('tpope/vim-fugitive')
 end)
