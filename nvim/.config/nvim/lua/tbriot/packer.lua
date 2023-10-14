319739c269f2863f437f1128efd726553d6aebb3
Thomas Briot <thomas.briot82@gmail.com>
2023-10-14 14:46:22 -0400

    Add lsp-zero config to nvim packer file

--- nvim/.config/nvim/lua/tbriot/packer.lua
+++ nvim/.config/nvim/lua/tbriot/packer.lua
@@ -4,23 +4,40 @@
 vim.cmd [[packadd packer.nvim]]
 --
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use({ 'rose-pine/neovim', as = 'rose-pine' ,
+  config = function()
+	  vim.cmd('colorscheme rose-pine')
+  end
+     })
+
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.3',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment these if you want to manage LSP servers from neovim
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
     }
-
-    use({ 'rose-pine/neovim', as = 'rose-pine' ,
-    config = function()
-        vim.cmd('colorscheme rose-pine')
-    end
-})
-
-use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-use('theprimeagen/harpoon')
-use('mbbill/undotree')
-use('tpope/vim-fugitive')
 end)
