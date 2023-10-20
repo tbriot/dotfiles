ecf9089c52f6f92111d4d2ee36e544bd5176e6cf
Thomas Briot <thomas.briot82@gmail.com>
2023-10-19 22:15:10 -0400

    Fix cheatsheet script

--- bin/.local/scripts/tmux-cht.sh
+++ bin/.local/scripts/tmux-cht.sh
@@ -8,7 +8,7 @@ read -p "Enter Query: " query
 
 if grep -qs "$selected" $XDG_CONFIG_HOME/tmux/tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    tmux neww zsh -c "curl -s cht.sh/$selected/$query | less -R"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+    tmux neww zsh -c "curl -s cht.sh/$selected~$query | less -R"
 fi
