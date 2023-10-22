da909929b80aae73049995dcd1194fc77e82a44a
Thomas Briot <thomas.briot82@gmail.com>
2023-10-22 13:19:28 -0400

    Change aliases for ls

--- zsh/.config/zsh/aliases.sh
+++ zsh/.config/zsh/aliases.sh
@@ -1,13 +1,13 @@
 ######################################################################################
 # 			Aliases
 ######################################################################################
-alias vim='nvim'
 alias ls='ls --color=auto'
+alias ll='ls -l --almost-all --classify --human-readable'
+alias l='ll'
+
+alias vim='nvim'
+alias v='vim'
 
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
-
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
