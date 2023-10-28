cc87c21ac8d625ed134f4bd6839b72899c727714
Thomas Briot <thomas.briot82@gmail.com>
2023-10-27 20:34:30 -0400

    Add 'gitgutter' nvim plugin

--- nvim/.config/nvim/lua/tbriot/packer.lua
+++ nvim/.config/nvim/lua/tbriot/packer.lua
@@ -8,36 +8,37 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+      'nvim-telescope/telescope.nvim', tag = '0.1.3',
+      -- or                            , branch = '0.1.x',
+      requires = { {'nvim-lua/plenary.nvim'} }
   }
 
   use({ 'rose-pine/neovim', as = 'rose-pine' ,
   config = function()
-	  vim.cmd('colorscheme rose-pine')
+      vim.cmd('colorscheme rose-pine')
   end
      })
 
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-    use('theprimeagen/harpoon')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
+     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+     use('theprimeagen/harpoon')
+     use('mbbill/undotree')
+     use('tpope/vim-fugitive')
+     use('airblade/vim-gitgutter')
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        requires = {
-            --- Uncomment these if you want to manage LSP servers from neovim
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+     use {
+         'VonHeikemen/lsp-zero.nvim',
+         branch = 'v3.x',
+         requires = {
+             --- Uncomment these if you want to manage LSP servers from neovim
+             {'williamboman/mason.nvim'},
+             {'williamboman/mason-lspconfig.nvim'},
 
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'L3MON4D3/LuaSnip'},
-        }
+             -- LSP Support
+             {'neovim/nvim-lspconfig'},
+             -- Autocompletion
+             {'hrsh7th/nvim-cmp'},
+             {'hrsh7th/cmp-nvim-lsp'},
+             {'L3MON4D3/LuaSnip'},
+         }
     }
 end)
