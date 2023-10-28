51997a7f71aa0c8fa7771e31cffdd377e15cbec5
Thomas Briot <thomas.briot82@gmail.com>
2023-10-28 08:36:15 -0400

    Add ~/.dotfiles to fzf list

--- bin/.local/scripts/tmux-sessionizer.sh
+++ bin/.local/scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/development/tbriot -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$({ find ~/development/tbriot -mindepth 1 -maxdepth 1 -type d; echo ~/.dotfiles } | fzf)
 fi
 
 if [[ -z $selected ]]; then
