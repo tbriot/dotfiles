11063a337d63490ac43006ff2bbe88e7f2ee896e
Thomas Briot <thomas.briot82@gmail.com>
2023-10-29 12:21:33 -0400

    Configure lsp and autocomplete

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -7,6 +7,13 @@ lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
   lsp_zero.default_keymaps({buffer = bufnr})
+
+  local opts = {buffer = bufnr}
+
+  -- Format buffer using a keybinding
+  vim.keymap.set({'n', 'x'}, 'gq', function()
+      vim.lsp.buf.format({async = false, timeout_ms = 10000})
+  end, opts)
 end)
 
 -- Install language servers
@@ -26,22 +33,36 @@ require('lspconfig').terraformls.setup({})
 --  Autocompletion
 ---------------------
 local cmp = require('cmp')
+local cmp_format = require('lsp-zero').cmp_format()
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    preselect = 'item',
+    completion = {
+        completeopt = 'menu,menuone,noinsert'
+    },
+    sources = {
+        {name = 'nvim_lsp'}
+    },
+    mapping = cmp.mapping.preset.insert({
+        -- `Enter` key to confirm completion
+        ['<CR>'] = cmp.mapping.confirm({select = true}),
 
-    -- Ctrl+Space to trigger completion menu
-    ['<C-Space>'] = cmp.mapping.complete(),
+        -- Ctrl+Space to trigger completion menu
+        ['<C-Space>'] = cmp.mapping.complete(),
 
-    -- Navigate between snippet placeholder
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+        -- Regular tab complete
+        ['<Tab>'] = cmp_action.tab_complete(),
+        ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
 
-    -- Scroll up and down in the completion documentation
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-  })
+        -- Navigate between snippet placeholder
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+        -- Scroll up and down in the completion documentation
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    }),
+    -- Show source name in completion menu
+    formatting = cmp_format,
 })
