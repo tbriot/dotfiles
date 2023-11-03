1bee132280f27df92d861fa0b1df25344258b452
Thomas Briot <thomas.briot82@gmail.com>
2023-11-03 11:16:49 -0400

    fix(fzf): manage one extra level in project hierarchy when fzfing

--- bin/.local/scripts/tmux-sessionizer.sh
+++ bin/.local/scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$({ find ~/development/tbriot -mindepth 1 -maxdepth 1 -type d; echo ~/.dotfiles } | fzf)
+    selected=$({ find ~/development -mindepth 2 -maxdepth 2 -type d; echo ~/.dotfiles } | fzf)
 fi
 
 if [[ -z $selected ]]; then
