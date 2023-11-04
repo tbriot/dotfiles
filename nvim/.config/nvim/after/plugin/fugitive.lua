d71de5c06f603d7057f7de2475e05af6ac098a2c
Thomas Briot <thomas.briot82@gmail.com>
2023-11-04 15:38:21 -0400

    feat(nvim): add shortcut <leader>g to open fugitive Git cmd in vert pane

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,1 +1,2 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+-- Open Fugitive Git status in vertical split, on the right of the window
+vim.keymap.set("n", "<leader>g", "<cmd>vertical rightbelow Git<CR>");
