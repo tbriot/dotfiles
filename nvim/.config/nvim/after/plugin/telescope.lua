cfe1f1091720a9fbaaf9a16ab2598f36101639a5
Thomas Briot <thomas.briot82@gmail.com>
2023-12-09 15:09:32 -0500

    feat(neovim): add keymap for telescope marks picker

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -3,6 +3,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.git_branches, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fm', builtin.marks, {})
 -- List Built-in pickers
 vim.keymap.set('n', '<leader>fp', builtin.builtin, {})
 -- List buffers
