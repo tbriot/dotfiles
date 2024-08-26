6dfbdcb1352561aaf9882596a1e58fd3d9860eb5
Thomas Briot <thomas.briot82@gmail.com>
2024-08-25 20:31:32 -0500

    feat: add lualine plugin

---
+++ nvim/.config/nvim/lua/lazy-plugins-specs/lualine.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      options = {
+        theme = "auto",
+        icons_enabled = true,
+        component_separators = "|",
+        section_separators = "",
+      },
+    },
+  },
+}
