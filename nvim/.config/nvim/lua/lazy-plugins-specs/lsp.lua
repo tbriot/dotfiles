91d82b4e4c17a9170127fa898e02941dcda8ad7a
Thomas Briot <thomas.briot82@gmail.com>
2024-09-15 13:51:47 -0400

    fix(neovim): have lsp recognize 'vim' global

--- nvim/.config/nvim/lua/lazy-plugins-specs/lsp.lua
+++ nvim/.config/nvim/lua/lazy-plugins-specs/lsp.lua
@@ -40,5 +40,17 @@ return {
                 capabilities = capabilities,
             }
         end
+
+        require('lspconfig').lua_ls.setup {
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        -- Get the language server to recognize the `vim` global
+                        globals = { 'vim' },
+                    },
+                },
+            },
+        }
     end,
 }
