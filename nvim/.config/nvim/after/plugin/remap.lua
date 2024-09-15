e8cf420fd0b8e1f4ed1bb1934e613b6f3aaf7002
Thomas Briot <thomas.briot82@gmail.com>
2024-09-15 09:48:06 -0400

    feat(neovim): rework harpoon config and shortcuts

--- nvim/.config/nvim/after/plugin/remap.lua
+++ nvim/.config/nvim/after/plugin/remap.lua
@@ -25,10 +25,10 @@ end)
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- Yank in system clipboard register
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 -- Delete to void register
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
@@ -37,12 +37,6 @@ vim.keymap.set("n", "Q", "<nop>")
 -- Change project
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer.sh<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
--- Quick fix navigation
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- Starts substitute command to replace current word
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
